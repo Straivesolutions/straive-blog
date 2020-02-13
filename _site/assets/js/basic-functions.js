6d1e41c8691c79cada9e3c6d727a2cef24a0ae8a
gravewalker666 <heshan.andris.pro@gmail.com>
2020-02-13 13:49:58 +0530

    Add share link copy button

--- _site/assets/js/basic-functions.js
+++ _site/assets/js/basic-functions.js
@@ -1,3 +1,15 @@
 function goBack() {
     window.history.back();
 }
+
+function copyText(){
+    var dummy = document.createElement('input'),
+        text = window.location.href;
+
+    document.body.appendChild(dummy);
+    dummy.value = text;
+    dummy.select();
+    document.execCommand('copy');
+    document.body.removeChild(dummy);
+    alert("copied!!")
+}
