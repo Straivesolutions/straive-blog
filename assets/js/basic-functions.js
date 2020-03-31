0f870289a28bc483da28005212d82a1e9069a16e
gravewalker666 <heshan.andris.pro@gmail.com>
2020-03-31 18:48:14 +0530

    Add disqus comment snippet

--- assets/js/basic-functions.js
+++ assets/js/basic-functions.js
@@ -23,3 +23,11 @@ function activeNightMode() {
         $('#post_body').addClass('night-mode');
     }
 }
+
+
+(function() { // DON'T EDIT BELOW THIS LINE
+    var d = document, s = d.createElement('script');
+    s.src = 'https://fiction-times.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+})();
